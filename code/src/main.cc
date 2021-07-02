d75b6b28b87720b3c48141f87d4b33d79b5c53e1
FelipeLevinir <levinir.f@gmail.com>
2021-07-01 20:57:47 -0400

    Update main.cc

--- code/src/main.cc
+++ code/src/main.cc
@@ -107,7 +107,7 @@ int main(int argc, char *argv[])
 	// definición de mpi::reduce
 	//  https://www.boost.org/doc/libs/1_71_0/doc/html/boost/mpi/reduce.html
     
-	boost::mpi::reduce(world, *numbersToSum, sumaParalela, std::plus<float>(), senderRank);
+	boost::mpi::reduce(world, sumaParalela, *numbersToSum, std::plus<float>(), senderRank);
 
 	//Finalmente, el proceso 0 muestra la suma paralela y el tiempo que tomó
 	//realizarla.
